ffeaf66591eaaaf2ba0245b5912a5aadd55e0bf4
lucas_elpechito@hotmail.com <lucas_elpechito@hotmail.com@ac7c5744-db18-faa8-7151-e89621f7a6a3>
2010-10-07 19:46:14 +0000

    implementacion suma y resta si tiene los grados ==

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -7,20 +7,37 @@ public class Calculadora {
 	
 	
 	public static Polinomio suma(Polinomio p1,Polinomio p2){
+		p1=p1.ordenarDec(p1);
+		p2=p2.ordenarDec(p2);
+		p1=p1.completar(p1);
+		p1.verPolinomio();
+		p2=p2.completar(p2);
+		p2.verPolinomio();
 		Polinomio resultado=new Polinomio();
-		
+		if(p1.grado(p1)==p2.grado(p2)){
+			for(int i=0;i<p1.getPolinomio().size();i++){
+				resultado.addTermPolinomico(p1.getPolinomio().get(i).getValor()+p2.getPolinomio().get(i).getValor(),p1.getPolinomio().get(i).getExp());
+			}
+		}
 		return resultado;
 	}
     
 	public static Polinomio resta(Polinomio p1,Polinomio p2){
-		Termino t3=new Termino();
-		Polinomio p3=new Polinomio();
-		Termino t1=p1.getPolinomio().get(0);
-		Termino t2=p2.getPolinomio().get(0);
-		t3.setValor(t1.getValor()-t2.getValor());
-		t3.setExp(t1.getExp());
-		p3.addTermPolinomico(t3.getValor(),t3.getExp());
-		return p3;
+		p1=p1.ordenarDec(p1);
+		p2=p2.ordenarDec(p2);
+		p1=p1.completar(p1);
+		p1.verPolinomio();
+		System.out.println("\n");
+		p2=p2.completar(p2);
+		p2.verPolinomio();
+		System.out.println("\n");
+		Polinomio resultado=new Polinomio();
+		if(p1.grado(p1)==p2.grado(p2)){
+			for(int i=0;i<p1.getPolinomio().size();i++){
+				resultado.addTermPolinomico(p1.getPolinomio().get(i).getValor()-p2.getPolinomio().get(i).getValor(),p1.getPolinomio().get(i).getExp());
+			}
+		}
+		return resultado;
 	}
 	
 	public static Polinomio producto(Polinomio p1,Polinomio p2){
